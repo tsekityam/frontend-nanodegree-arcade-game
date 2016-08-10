e65322d37228b8083527bc207c748a1016fc4b0f
Tse Kit Yam <me@kytse.com>
2016-08-10 21:17:47 +0800

    feat: add characters to the game

--- js/engine.js
+++ js/engine.js
@@ -91,10 +91,13 @@ var Engine = (function(global) {
      * render methods.
      */
     function updateEntities(dt) {
+        selector.update();
         allEnemies.forEach(function(enemy) {
             enemy.update(dt);
         });
-        player.update();
+        allPlayers.forEach(function(player) {
+            player.update();
+        });
     }
 
     /* This function initially draws the "game level", it will then call
@@ -147,11 +150,13 @@ var Engine = (function(global) {
         /* Loop through all of the objects within the allEnemies array and call
          * the render function you have defined.
          */
+        selector.render();
         allEnemies.forEach(function(enemy) {
             enemy.render();
         });
-
-        player.render();
+        allPlayers.forEach(function(player) {
+            player.render();
+        });
     }
 
     /* This function does nothing but it could have been a good place to
@@ -171,7 +176,12 @@ var Engine = (function(global) {
         'images/water-block.png',
         'images/grass-block.png',
         'images/enemy-bug.png',
-        'images/char-boy.png'
+        'images/char-boy.png',
+        'images/char-cat-girl.png',
+        'images/char-horn-girl.png',
+        'images/char-pink-girl.png',
+        'images/char-princess-girl.png',
+        'images/Selector.png',
     ]);
     Resources.onReady(init);
 
