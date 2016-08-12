9e3652bef206848abd01a9b5c77d531f3891c595
Tse Kit Yam <me@kytse.com>
2016-08-12 23:18:48 +0800

    Revert "refactor: update the logic of board render"
    This reverts commit a7ad620c14b4bf2ecdadd97f368c6b4d5dc08065.

--- js/engine.js
+++ js/engine.js
@@ -25,8 +25,8 @@ var Engine = (function(global) {
         ctx = canvas.getContext('2d'),
         lastTime;
 
-    canvas.width = numCols * colWidth;
-    canvas.height = numRows * rowHeight + 108;
+    canvas.width = 505;
+    canvas.height = 606;
     doc.body.appendChild(canvas);
 
     /* This function serves as the kickoff point for the game loop itself
